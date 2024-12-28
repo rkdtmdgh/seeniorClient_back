7c15e16685b2893d5d8a8a9f7673d32e097b34ec
llsssyy <souldkdl93@gamil.com>
2024-12-28 21:10:13 +0900

    241228 care_list_category logic add

---
+++ src/main/java/com/see_nior/seeniorClient/dto/CareListCategoryDto.java
@@ -0,0 +1,21 @@
+package com.see_nior.seeniorClient.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CareListCategoryDto {
+	
+	private int clc_no;
+	private int clc_user_no;
+	private String clc_name;
+	private int clc_is_deleted;
+	private String clc_reg_date;
+	private String clc_mod_date;
+	
+	private UserAccountDto userAccountDto;
+
+}
