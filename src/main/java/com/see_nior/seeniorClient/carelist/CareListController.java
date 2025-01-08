c6872faacae3b491a3dfbb20970a2595a7b8ce96
llsssyy <souldkdl93@gamil.com>
2025-01-08 15:48:34 +0900

    check

--- src/main/java/com/see_nior/seeniorClient/carelist/CareListController.java
+++ src/main/java/com/see_nior/seeniorClient/carelist/CareListController.java
@@ -17,7 +17,7 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @RestController
-@RequestMapping("/care-list")
+@RequestMapping("/care_list")
 @RequiredArgsConstructor
 public class CareListController {
 
