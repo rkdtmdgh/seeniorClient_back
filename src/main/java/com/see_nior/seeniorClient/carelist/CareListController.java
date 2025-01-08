74f0750c2e3ee4853a616908090a91eb7d971717
llsssyy <souldkdl93@gamil.com>
2025-01-08 23:26:22 +0900

    250108 getCareList add

--- src/main/java/com/see_nior/seeniorClient/carelist/CareListController.java
+++ src/main/java/com/see_nior/seeniorClient/carelist/CareListController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.see_nior.seeniorClient.dto.CareListCategoryDto;
-import com.see_nior.seeniorClient.dto.CareListDto;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -27,10 +26,10 @@ public class CareListController {
 	
 	// 케어리스트 카테고리명 중복 확인
 	@GetMapping("/cate_info/is_carelist_category")
-	public boolean isCareListCategory(@RequestParam(value = "clc_name") String clc_name) {
+	public boolean isCareListCategory(@RequestParam(value = "clc_name") String clc_name, Principal principal) {
 		log.info("isCareListCategory()");
 		
-		boolean isCareListCategory = careListService.isCareListCategory(clc_name);
+		boolean isCareListCategory = careListService.isCareListCategory(clc_name, principal.getName());
 		
 		return isCareListCategory;
 		
@@ -49,10 +48,10 @@ public class CareListController {
 	
 	// 모든 케어리스트 카테고리 가져오기 (케어리스트에서 <select> 박스)
 	@GetMapping("/cate_info/get_category_list_select")
-	public Object getCategoryListSelect() {
+	public Object getCategoryListSelect(Principal principal) {
 		log.info("getCategoryListSelect()");
 		
-		Map<String, Object> careListCategoryDtos = careListService.getCategoryList();
+		Map<String, Object> careListCategoryDtos = careListService.getCategoryList(principal.getName());
 		
 		return careListCategoryDtos;
 		
@@ -62,17 +61,16 @@ public class CareListController {
 	@GetMapping("/cate_info/get_category_list")
 	public Object getCategoryList(
 			@RequestParam(value = "page_limit") int page_limit,
-			@RequestParam(value = "block_limit") int block_limit,
 			@RequestParam(value = "page", required = false, defaultValue = "1") int page,
-			@RequestParam(value = "sortValue", required = false, defaultValue = "clc_no") String sortValue,
-			@RequestParam(value = "order", required = false, defaultValue = "desc") String order) {
+			@RequestParam(value = "sortValue", required = false, defaultValue = "clc_name") String sortValue,
+			@RequestParam(value = "order", required = false, defaultValue = "asc") String order, Principal principal) {
 		log.info("getCategoryList()");
 		
 		//페이지 번호에 따른 케어리스트 카테고리 리스트들 가져오기
-		Map<String, Object> careListCategoryListWithPage = careListService.getCareListCategoryListWithPage(page_limit, sortValue, order, page);
+		Map<String, Object> careListCategoryListWithPage = careListService.getCareListCategoryListWithPage(page_limit, sortValue, order, page, principal.getName());
 		
 		// 케어리스트 카테고리 총 페이지 개수 가져오기
-		Map<String, Object> careListCategoryListPageNum = careListService.getCareListCategoryListPageNum(page_limit, block_limit, page);
+		Map<String, Object> careListCategoryListPageNum = careListService.getCareListCategoryListPageNum(page_limit, page, principal.getName());
 		
 		careListCategoryListWithPage.put("careListCategoryListPageNum", careListCategoryListPageNum);
 		careListCategoryListWithPage.put("sortValue", sortValue);
@@ -106,8 +104,9 @@ public class CareListController {
 	
 //////////////////////////////////////////////////// 케어리스트
 	
+/*	
 	// 케어리스트 등록하기
-	@PostMapping("info/create_confirm")
+	@PostMapping("/info/create_confirm")
 	public boolean createConfirm(CareListDto careListDto) {
 		log.info("createConfirm()");
 		
@@ -115,12 +114,56 @@ public class CareListController {
 //		
 //		return createResult;
 		
-		return false;
+	}
+ */
+	
+	// 모든 케어리스트 가져오기
+	@GetMapping("/info/get_care_list")
+	public Object getCareList(
+			@RequestParam(value = "page_limit") int page_limit,
+			@RequestParam(value = "page", required = false, defaultValue = "1") int page,
+			@RequestParam(value = "sortValue", required = false, defaultValue = "cl_no") String sortValue,
+			@RequestParam(value = "order", required = false, defaultValue = "desc") String order, Principal principal) {
+		log.info("getCareList()");
+		
+		// 페이지 번호에 따른 케어리스트 가져오기
+		Map<String, Object> careListWithPage = careListService.getCareListWithPage(page_limit, page, sortValue, order, principal.getName());
+		
+		// 케어리스트 총 페이지 개수 가져오기
+		Map<String, Object> careListPageNum = careListService.getCareListPageNum(page_limit, page, principal.getName());
+		
+		careListWithPage.put("careListPageNum", careListPageNum);
+		careListWithPage.put("sortValue", sortValue);
+		careListWithPage.put("order", order);
+		
+		return careListWithPage;
 		
 	}
 	
-	
-	// 케어리스트 불러오기
+	// 카테고리별 케어리스트 가져오기
+	@GetMapping("/info/get_care_list_by_category")
+	public Object getCareListByCategory(
+			@RequestParam(value = "page_limit") int page_limit,
+			@RequestParam(value = "page", required = false, defaultValue = "1") int page,
+			@RequestParam(value = "sortValue", required = false, defaultValue = "cl_no") String sortValue,
+			@RequestParam(value = "order", required = false, defaultValue = "desc") String order,
+			@RequestParam(value = "infoNo") int infoNo, Principal principal) {
+		log.info("getCareListByCategory()");
+		
+		// 페이지 번호에 따른 카테고리별 케어리스트 가져오기
+		Map<String, Object> careListByCategoryWithPage = careListService.getCareListByCategoryWithPage(page_limit, page, sortValue, order, infoNo, principal.getName());
+		
+		// 카테고리별 케어리스트 총 페이지 개수 가져오기
+		Map<String, Object> careListByCategoryPageNum = careListService.getCareListByCategoryPageNum(page_limit, page, infoNo, principal.getName());
+		
+		careListByCategoryWithPage.put("careListByCategoryPageNum", careListByCategoryPageNum);
+		careListByCategoryWithPage.put("sortValue", sortValue);
+		careListByCategoryWithPage.put("order", order);
+		careListByCategoryWithPage.put("infoNo", infoNo);
+		
+		return careListByCategoryWithPage;
+		
+	}
 	
 	
 	// 케어리스트 수정하기
