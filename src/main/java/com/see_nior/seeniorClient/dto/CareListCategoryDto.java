12887c5fd3d96851bb8ace5f71fbaa46eaebb9c1
llsssyy <souldkdl93@gamil.com>
2025-01-12 00:16:17 +0900

    250112 carelist modify

--- src/main/java/com/see_nior/seeniorClient/dto/CareListCategoryDto.java
+++ src/main/java/com/see_nior/seeniorClient/dto/CareListCategoryDto.java
@@ -12,6 +12,7 @@ public class CareListCategoryDto {
 	private int clc_no;
 	private int clc_user_no;
 	private String clc_name;
+	private int clc_item_cnt;
 	private int clc_is_deleted;
 	private String clc_reg_date;
 	private String clc_mod_date;
