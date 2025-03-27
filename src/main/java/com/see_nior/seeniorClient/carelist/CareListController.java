36fb585aa53cbb0176a36f957cd072be09353944
DESKTOP-QAICPE9\c <rkdtmdgh3@naver.com>
2025-03-27 19:51:59 +0900

    20250327 carelistcontroller check

--- src/main/java/com/see_nior/seeniorClient/carelist/CareListController.java
+++ src/main/java/com/see_nior/seeniorClient/carelist/CareListController.java
@@ -150,15 +150,13 @@ public class CareListController {
 	// 케어리스트 즐겨찾기 ON / OFF
 	@PostMapping("/info/favorites_confirm")
 	public boolean favoritesConfirm(
-//			@RequestParam(value = "cl_no") int cl_no,
-//			@RequestParam(value = "cl_favorites") int cl_favorites
-			@RequestParam CareListDto careListDto
-			) {
-		log.info("favoritesConfirm() -------- {}", careListDto);
+			@RequestParam(value = "cl_no") int cl_no,
+			@RequestParam(value = "cl_favorites") int cl_favorites) {
+		log.info("favoritesConfirm()");
 		
-//		boolean favoritesResult = careListService.favorites_confirm(cl_no, cl_favorites);
+		boolean favoritesResult = careListService.favorites_confirm(cl_no, cl_favorites);
 		
-		return false;
+		return favoritesResult;
 		
 	}
 
