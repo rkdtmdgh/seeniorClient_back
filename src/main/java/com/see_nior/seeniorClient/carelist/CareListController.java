196f1105b2a356269bdfb61bfd55c2c1db6d0ce7
llsssyy <souldkdl93@gamil.com>
2025-04-02 14:29:28 +0900

    250402 careListDelete fix

--- src/main/java/com/see_nior/seeniorClient/carelist/CareListController.java
+++ src/main/java/com/see_nior/seeniorClient/carelist/CareListController.java
@@ -259,10 +259,10 @@ public class CareListController {
 	
 	// 케어리스트 삭제하기
 	@PostMapping("/info/delete_care_list_confirm")
-	public boolean deleteCareListConfirm(@RequestParam(value = "cl_no") int cl_no) {
+	public boolean deleteCareListConfirm(@RequestParam(value = "cl_no") int cl_no, Principal principal) {
 		log.info("deleteCareListConfirm()");
 		
-		boolean deleteCareListResult = careListService.deleteCareListConfirm(cl_no);
+		boolean deleteCareListResult = careListService.deleteCareListConfirm(cl_no, principal.getName());
 		
 		return deleteCareListResult;
 		
