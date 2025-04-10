857e096f215058ead25f4adc3be96d441d6f62a9
DESKTOP-QAICPE9\c <rkdtmdgh3@naver.com>
2025-04-10 19:53:42 +0900

    20250410 user_mapper updateUserAccount fix

--- src/main/java/com/see_nior/seeniorClient/user/UserController.java
+++ src/main/java/com/see_nior/seeniorClient/user/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -119,7 +118,6 @@ public class UserController {
 			
 			return responseMap;
 		}
-			
 
 		// 프로필 변경이 없는 경우
 		if (files == null || files.isEmpty()) {
